0ec866e9625edf585e9b6aecb20976c362baccd6
emanAbd1 <emanabdulsemed4398@gmail.com>
2026-07-05 16:34:18 +0000

    creat counter mathematical

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ var counterParagraph = document.getElementById("counter");
 var clearBtn = document.getElementById("clearBtn");
 var allDoneMsg = document.getElementById("allDoneMsg");
 var colorCircles = document.querySelectorAll(".color-circle");
+var tasks = [];
+function updateCounters() {
+  var totalTasks = tasks.length;
+  var undoneCount = 0;
+
+  for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].done === false) {
+      undoneCount = undoneCount + 1;
+    }
+  }
+
+  var completedCount = totalTasks - undoneCount;
+
+  remainingCount.textContent = undoneCount + " (" + completedCount + " of " + totalTasks + " completed)";
+
+  if (totalTasks > 0 && undoneCount === 0) {
+    counterParagraph.innerHTML = '🎉 <span id="remainingCount" style="color: #388e3c; font-weight: bold;">All tasks done!</span>';
+    allDoneMsg.classList.add("visible");
+  } else {
+    counterParagraph.innerHTML = 'Tasks remaining: <span id="remainingCount">' + undoneCount + '</span> (' + completedCount + ' of ' + totalTasks + ' completed)';
+    allDoneMsg.classList.remove("visible"); // Hide celebration text block
+  }
+}
